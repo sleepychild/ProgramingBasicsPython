b681ae2a448a06fccc5f212e428fa26e7b23c7e7
Mario Kukucov <sleepychild@abv.bg>
2021-10-23 23:18:00 +0300

    Create 05_Travelling.py

---
+++ NestedLoops/LAB/05_Travelling.py
@@ -0,0 +1,11 @@
+destination: str = str()
+cost: float = float()
+
+while True:
+    destination = input()
+    if destination == 'End':
+        exit(0)
+    cost = float(input())
+    while cost >= 0:
+        cost -= float(input())
+    print(f'Going to {destination}!')
