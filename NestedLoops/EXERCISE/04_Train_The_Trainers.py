30af239960571c2597b51b8d7236e91b82cf67aa
Mario Kukutsov <mario.kukutsov@sap.com>
2021-10-30 08:11:13 +0300

    Update 04_Train_The_Trainers.py
    Up Next 05. Special Numbers

--- NestedLoops/EXERCISE/04_Train_The_Trainers.py
+++ NestedLoops/EXERCISE/04_Train_The_Trainers.py
@@ -3,16 +3,22 @@ trainers_count: int = int(input())
 input_data: str = str()
 
 course_name: str = str()
-course_avg: float = float()
+course_grades: list = list()
 courses_avgs: list = list()
 final_avg: float = float()
+
+output_data: str = str()
 
 while True:
     input_data = input()
     if input_data == 'Finish':
         break
     course_name = input_data
-    
+    course_grades = list()
+    for _ in range(trainers_count):
+        course_grades.append(float(input()))
+    courses_avgs.append( sum(course_grades) / len(course_grades) )
+    output_data += f'{course_name} - {courses_avgs[-1]:.2f}.\n'
 
-print(f'{course_name} - {course_avg:.2f}.')
-print(f'Student\'s final assessment is { sum(courses_avgs) / len(courses_avgs) :.2f}.')
+output_data += f'Student\'s final assessment is { sum(courses_avgs) / len(courses_avgs) :.2f}.'
+print(output_data)
