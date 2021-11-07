696513deddbd0783e9db93ff793c96d898ad8982
Mario Kukutsov <mario.kukutsov@sap.com>
2021-11-07 13:40:36 +0200

    Update 06_Cinema_Tickets.py
    Judge labs and exercises complete.

--- NestedLoops/EXERCISE/06_Cinema_Tickets.py
+++ NestedLoops/EXERCISE/06_Cinema_Tickets.py
@@ -7,43 +7,51 @@ class CinemaClass:
         'kid',
     )
 
+    def show_screenings(self) -> None:
+            print(self.screenings[-1])
+
     def screen_movie(self, name: str, seats: int) -> None:
         self.current_screening = name
-        self.screenings.append({ScreeningClass(name, seats)})
+        self.screenings.append(ScreeningClass(name, seats))
 
-    def sell_ticket(self, ticket_type: str) -> None:
-        self.screenings[-1].tickets[self.tickets_types.index(ticket_type)] += 1
+    def sell_ticket(self, ticket_type: str) -> bool:
+        return self.screenings[-1].sell_ticket(self.tickets_types.index(ticket_type))
 
     def __str__(self) -> str:
         class_string: str = str()
         ticket_type_index: int
         total_tickets: list = [0] * 3
-        for movie, screening in self.screenings.items():
-            class_string += f'{movie} - {screening.get_seats_full_percent():.2f}% full.\n'
+        for screening in self.screenings:
             for ticket_type in self.tickets_types:
                 ticket_type_index = self.tickets_types.index(ticket_type)
                 total_tickets[ticket_type_index] += screening.tickets[ticket_type_index]
 
-        class_string += f'Total tickets: {sum(total_tickets)}\n'
-        # 66.67% student tickets.
-        # 25.00% standard tickets.
-        # 8.33% kids tickets.
+        total_tickets_sum: int = sum(total_tickets)
+        class_string += f'Total tickets: {total_tickets_sum}\n'
+        class_string += f'{ total_tickets[0] / total_tickets_sum * 100 :.2f}% student tickets.\n'
+        class_string += f'{ total_tickets[1] / total_tickets_sum * 100 :.2f}% standard tickets.\n'
+        class_string += f'{ total_tickets[2] / total_tickets_sum * 100 :.2f}% kids tickets.'
         return class_string
 
 class ScreeningClass:
     name: str
     seats: int
-    tickets: list = [0] * 3
+    tickets: list
 
     def __init__(self, name: str, seats: int) -> None:
         self.name = name
         self.seats = seats
+        self.tickets = [0] * 3
+
+    def sell_ticket(self, ticket_type_index: int) -> bool:
+        self.tickets[ticket_type_index] += 1
+        return sum(self.tickets) == self.seats
 
     def get_seats_full_percent(self) -> float:
         return sum(self.tickets) / self.seats * 100
     
     def __str__(self) -> str:
-        return f'{self.name} - {self.seats} - {self.tickets}'
+        return f'{self.name} - {self.get_seats_full_percent():.2f}% full.'
 
 if __name__ == '__main__':
     cinema: CinemaClass = CinemaClass()
@@ -51,13 +59,20 @@ if __name__ == '__main__':
     while True:    
         input_data = input()
         if input_data == 'Finish':
-            break
+            print(cinema)
+            exit(0)
         movie_name = input_data
         cinema.screen_movie(movie_name, int(input()))
         while True:
             input_data = input()
             if input_data == 'End':
+                cinema.show_screenings()
                 break
-            cinema.sell_ticket(input_data)
-
-    print(cinema)
+            elif input_data == 'Finish':
+                cinema.show_screenings()
+                print(cinema)
+                exit(0)
+            else:
+                if cinema.sell_ticket(input_data):
+                    cinema.show_screenings()
+                    break
