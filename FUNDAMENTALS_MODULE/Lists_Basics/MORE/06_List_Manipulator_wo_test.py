dc1fa787047cbe18c83e5413de1e0fa0166e5ee2
Mario Kukutsov <mario.kukutsov@sap.com>
2022-02-05 00:06:59 +0200

    Create 06_List_Manipulator_wo_test.py

---
+++ FUNDAMENTALS_MODULE/Lists_Basics/MORE/06_List_Manipulator_wo_test.py
@@ -0,0 +1,104 @@
+from typing import List, Callable, Tuple
+
+
+class InvalidIndex(Exception):
+    def __str__(self) -> str:
+        return 'Invalid index'
+
+
+class InvalidCount(Exception):
+    def __str__(self) -> str:
+        return 'Invalid count'
+
+
+class NoMatches(Exception):
+    def __str__(self) -> str:
+        return 'No matches'
+
+
+class InteractiveList(list):
+
+    def __init__(self, list_in: List) -> None:
+        super().__init__(list_in)
+        self.length = len(self)
+
+    def exchange(self, *args) -> None:
+        index: int = int(args[0])
+        try:
+            self._index_check(index)
+        except InvalidIndex as e:
+            print(e)
+        else:
+            for _ in range(index+1):
+                self.append(self.pop(0))
+
+    def max(self, *args) -> None:
+        try:
+            print(self._max_min_even_odd(max, self.__getattribute__(f'_{args[0]}')))
+        except NoMatches as e:
+            print(e)
+
+    def min(self, *args) -> None:
+        try:
+            print(self._max_min_even_odd(min, self.__getattribute__(f'_{args[0]}')))
+        except NoMatches as e:
+            print(e)
+
+    def first(self, *args) -> None:
+        try:
+            print(self._first_last_even_odd(1, self.__getattribute__(f'_{args[1]}'), int(args[0])))
+        except InvalidCount as e:
+            print(e)
+    
+    def last(self, *args) -> None:
+        try:
+            print(self._first_last_even_odd(-1, self.__getattribute__(f'_{args[1]}'), int(args[0])))
+        except InvalidCount as e:
+            print(e)
+
+    def end(self) -> None:
+        print(self)
+        exit(0)
+
+    def _max_min_even_odd(self, mode: Callable[..., int], num_type: Callable[[int], bool]) -> int:
+        try:
+            t_val: int = mode(filter(num_type, self))
+        except ValueError as _:
+            raise NoMatches
+        else:
+            t_ind: int = int()
+            for _ in range(self.count(t_val)):
+                try:
+                    t_ind = self.index(t_val, t_ind+1)
+                except ValueError:
+                    pass
+            return t_ind
+
+    def _first_last_even_odd(self, mode: int, num_type: Callable[[int], bool], count: int) -> List:
+        if count > self.length :
+            raise InvalidCount
+        else:
+            return list(filter(num_type, self))[::mode][:count]
+
+    def _index_check(self, index: int) -> None:
+        if not 0 <= index < self.length:
+            raise InvalidIndex
+
+    @staticmethod
+    def _even(n: int) -> bool:
+        return (n % 2) == 0
+
+    @staticmethod
+    def _odd(n: int) -> bool:
+        return (n % 2) != 0
+
+    @classmethod
+    def list_from_string(cls, str_in: str, str_delimiter: str, element_type: type) -> 'InteractiveList':
+        return cls([ element_type(list_element) for list_element in str_in.split(str_delimiter)])
+
+
+if __name__ == '__main__':
+    interlist: InteractiveList = InteractiveList.list_from_string(input(), ' ', int)
+    while True:
+        cmd_prt: List[str] = input().split(' ')
+        interlist.__getattribute__(cmd_prt[0])(*cmd_prt[1:])
